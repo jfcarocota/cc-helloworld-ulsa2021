9ff3c9ecb8d3dc25eb45b1c36d217eeaa697e790
jfcarocota <jfcarocota@gmail.com>
2021-08-24 07:12:17 -0700

    sobrecarga

--- include/MyHeader.hh
+++ include/MyHeader.hh
@@ -1,4 +1,18 @@
+#include<string>
+
+//sobrecarga de metodo.
+
 int Add(int a, int b)
+{
+  return a + b;
+}
+
+float Add(float a, float b)
+{
+  return a + b;
+}
+
+std::string Add(std::string a, std::string b)
 {
   return a + b;
 }
--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,9 @@
 
 int main()
 {
-  std::cout << "Suma: " << Add(1, 2);
+  std::cout << "Suma: " << Add(1, 2) << std::endl;
+  std::cout << "Suma: " << Add(1.f, 2.5f) << std::endl;
+  std::cout << "Suma: " << Add("Hello", "World!") << std::endl;
   std::cin.get();
   return 0;
 }
